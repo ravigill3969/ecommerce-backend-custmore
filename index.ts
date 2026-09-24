86c0a6909dbb931c3d478c68be5d560de7cec200
ravigill3936 <gill1234ravi@gmail.com>
2026-09-24 11:11:40 -0400

    cors for socket.io fixed

--- index.ts
+++ index.ts
@@ -36,7 +36,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://ecommerce-frontend-customer.onrender.com",
     methods: ["GET", "POST"],
   },
 });
